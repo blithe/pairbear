d7cefc59fb1e1da617475cd633dd31a7600cea6a
Blithe Rocher <blitherocher@gmail.com>
2015-07-23 21:28:29 +0000

    Update environment

--- fastly/env.zsh
+++ fastly/env.zsh
@@ -1,1 +1,7 @@
 FASTLY=$PROJECTS/fastly/
+
+export FASTLY_DEV_BASE="/home/blithe/fastly"
+export GEM_HOME="$FASTLY_DEV_BASE"/var/gems
+export RUBYLIB="$FASTLY_DEV_BASE"/Pauper/lib:"$RUBYLIB"
+export PATH="$FASTLY_DEV_BASE/bin:$FASTLY_DEV_BASE"/Pauper/bin:"$PATH"
+export KNIFE_HOME="$FASTLY_DEV_BASE"/pauper-env/chef/
